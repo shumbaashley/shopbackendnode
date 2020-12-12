6a7afa30409300c2c3ff9a462265e01557937d62
shumbaashley <ashleytshumba@gmail.com>
2020-12-12 10:21:13 +0200

    set up db

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+
+const connectDB = require('./config/db')
+
+dotenv.config()
+
 const app = express();
 
+const authRouter = require('./routes/authRouter')
+
 app.use(express.json());
+app.use(morgan('dev'));
 
 
-app.get('/', (req, res) => {
-  res.json("Hello");
-});
+app.use('/api/auth', authRouter)
+  
+connectDB()
 
 const PORT = process.env.PORT || 5000
 
 
-app.listen(() => console.log(`Server running on port ${PORT}...`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`))
---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex : true })
+        console.log(`Connected to Database : ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error : ${error.message}`)
+        process.exit(1)
+    }
+
+}
+
+module.exports = connectDB
